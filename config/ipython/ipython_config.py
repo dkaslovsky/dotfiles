21fd06bcf9f0496171c291f0473556898496e57c
dkaslovsky <dkaslovsky@gmail.com>
2024-05-20 13:21:13 -0600

    ipython config overwrite error bg color

--- config/ipython/ipython_config.py
+++ config/ipython/ipython_config.py
@@ -1,6 +1,13 @@
 print("--------->>>>>>>> ENABLE AUTORELOAD <<<<<<<<<------------")
-
 c = get_config()
 c.InteractiveShellApp.exec_lines = []
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+
+print("--------->>>>>>>> ENABLE CUSTOM ERROR BACKGROUND COLOR <<<<<<<<<------------")
+try:
+    from IPython.core import ultratb
+    ultratb.VerboseTB._tb_highlight = "bg:ansired"
+except Exception as e:
+    print(f"Error patching background color for tracebacks: {e}")
+
